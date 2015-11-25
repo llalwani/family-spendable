9800c92df92b27c5a29988abb2892c73fc443241
Long Dao <longdao@studiodao.com>
2015-11-24 16:47:11 -0800

    changed table to ul

--- src/app/waitList/directives/itemTable.directive.js
+++ src/app/waitList/directives/itemTable.directive.js
@@ -1,44 +1,49 @@
-(function() {
-  'use strict';
+(function () {
+	'use strict';
 
-  angular
-    .module('app.waitList')
-    .directive('gzItemTable', gzItemTable);
+	angular
+		.module('app.waitList')
+		.directive('gzItemTable', gzItemTable);
 
-  function gzItemTable() {
-    return {
-      templateUrl: 'app/waitList/directives/itemTable.html',
-      restrict: 'E',
-      controller: ItemTableController,
-      controllerAs: 'vm',
-      bindToController: true,
-      scope: {
-        list: '='
-      }
-    }
-  }
+	function gzItemTable() {
+		return {
+			templateUrl: 'app/waitList/directives/itemTable.html',
+			restrict: 'E',
+			controller: ItemTableController,
+			controllerAs: 'vm',
+			bindToController: true,
+			scope: {
+				list: '='
+			}
+		}
+	}
 
-  ItemTableController.$inject = ['textMessageService'];
+	ItemTableController.$inject = ['textMessageService'];
 
-  function ItemTableController(textMessageService) {
-    var vm = this;
+	function ItemTableController(textMessageService) {
+		var vm = this;
 
-    vm.removeItem = removeItem;
-    vm.sendTextMessage = sendTextMessage;
-    vm.toggleDone = toggleDone;
+		vm.removeItem = removeItem;
+		vm.sendTextMessage = sendTextMessage;
+		vm.toggleDone = toggleDone;
+		vm.hover = hover;
 
-    function removeItem(item) {
-      vm.list.$remove(item);
-    }
+		function removeItem(item) {
+			vm.list.$remove(item);
+		}
 
-    function sendTextMessage(item) {
-      textMessageService.sendTextMessage(item, vm.list);
-    }
+		function sendTextMessage(item) {
+			textMessageService.sendTextMessage(item, vm.list);
+		}
 
-    function toggleDone(item) {
-      vm.list.$save(item);
-    }
+		function toggleDone(item) {
+			vm.list.$save(item);
+		}
 
-  }
+		function hover(item) {
+			// Shows/hides the delete button on hover
+			return item.showDelete = !item.showDelete;
+		};
+	}
 
 })();
