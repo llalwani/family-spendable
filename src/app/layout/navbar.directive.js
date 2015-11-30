a48e4754ba3eba0ce81d0c02f6353fd44382aeaf
Long Dao <longdao@studiodao.com>
2015-11-30 12:18:34 -0800

    collapse navbar

--- src/app/layout/navbar.directive.js
+++ src/app/layout/navbar.directive.js
@@ -18,6 +18,7 @@
   function NavBarController($location, authService) {
     var vm = this;
 
+		vm.isCollapsed = false;
     vm.isLoggedIn = authService.isLoggedIn;
     vm.login = authService.login;
     vm.logout = logout;
